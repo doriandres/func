5e27ee7bc94ab4e56abacd8d0f2e0e008441babc
Dorian Andrés <dcortesg441@ulacit.ed.cr>
2020-11-27 22:00:31 -0600

    Update func.js

--- func.js
+++ func.js
@@ -1,12 +1,12 @@
-function func(...args){
+function func(arguments){
     args = args.reverse();
     let fn = args[0];
     for(let dec of args.slice(1)) fn = dec(fn);
     return fn;
 }
 
-if(module){
-  module.exports = func;
-}else{
-  window.func = func;
+try{
+    module.exports = func;
+}catch(e){
+    window.func = func;
 }
