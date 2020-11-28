342e06c5a7937f2dd883bfd62eed408206134526
Dorian Andrés <dcortesg441@ulacit.ed.cr>
2020-11-27 21:51:26 -0600

    Create func.js

---
+++ func.js
@@ -0,0 +1,14 @@
+function func(...args){
+    args = args.reverse();
+    let fn = args[0];
+    for(let dec of args.slice(1)) fn = dec(fn);
+    return fn;
+}
+
+if(document && document.currentScript && document.currentScript.type === "module"){
+  exports func;
+}else if(module){
+  module.exports = func;
+}else{
+  window.func = func;
+}
